d1cad7ad4b049c2965b9eaadc22592e59433c634
Fazlı <fazlikilic62@gmail.com>
2024-12-05 23:33:23 +0300

    simply done

---
+++ app/src/main/java/com/example/sozlukuygulamasi/DictionaryApi.kt
@@ -0,0 +1,13 @@
+package com.example.sozlukuygulamasi
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DictionaryApi {
+
+    @GET("en/{word}")
+    suspend fun getMeaning(@Path("word") word : String) : Response<List<WordResult>>
+
+
+}
